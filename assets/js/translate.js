7b58287f3f63fd1683105730bfadaddb80bda68f
JAghazada <80761705+JAghazada@users.noreply.github.com>
2021-06-01 19:11:59 +0400

    ready

---
+++ assets/js/translate.js
@@ -0,0 +1,7 @@
+document.querySelector('.burger-box').addEventListener('click',function(){
+    
+    document.querySelector('.left-menu-section').classList.toggle('transfer')
+    document.querySelector('.left-menu-section').classList.toggle('let-place')
+    document.querySelector('.burger-div').classList.toggle('transfer-burger')
+
+})
